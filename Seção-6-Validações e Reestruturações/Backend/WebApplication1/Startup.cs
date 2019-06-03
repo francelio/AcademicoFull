a198dc85713f43a02b597f137dbb9e9f6b52dc77
Francelio Alencar <francelio.si@gmail.com>
2019-06-03 07:56:54 -0300

    inserindo Owin

---
+++ Seção-6-Validações e Reestruturações/Backend/WebApplication1/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Newtonsoft.Json.Serialization;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApp.Startup))]
+
+namespace WebApp
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			var config = new HttpConfiguration();
+			
+			config.MapHttpAttributeRoutes();
+			config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+
+			app.UseCors(CorsOptions.AllowAll);
+			app.UseWebApi(config);
+
+		}
+	}
+}
